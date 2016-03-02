60af4f2d4c24fef31d136dab3db3b52cbf63eaf4
mkmicik <mkmicik@ualberta.ca>
2016-03-01 20:43:52 -0700

    Modified authors template

--- social_dist/authors/views.py
+++ social_dist/authors/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-   return HttpResponse("Hello, world. You're at the authors index.")
+    return render(request, 'authors/index.html')
