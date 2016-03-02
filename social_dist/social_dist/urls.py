2ba57985cd64dcd2d567b70e04027c1f65b90837
mkmicik <mkmicik@ualberta.ca>
2016-03-02 14:37:30 -0700

    Changed dir structure to allow for static files

--- social_dist/social_dist/urls.py
+++ social_dist/social_dist/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^',include('authors.urls',namespace='authors')),
     url(r'^authors/',include('authors.urls',namespace='authors')),
+    url(r'^friends/',include('friends.urls',namespace='friends')),
     url(r'^posts/',include('posts.urls',namespace='posts')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^register/', include('authors.urls',namespace='authors')),
