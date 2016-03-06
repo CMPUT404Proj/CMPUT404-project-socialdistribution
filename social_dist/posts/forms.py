bee15cfa1e5af0734ede33f4985a58e27f372a04
kathb <kzb1895@gmail.com>
2016-03-06 15:55:17 -0700

    adding a post works finally

--- social_dist/posts/forms.py
+++ social_dist/posts/forms.py
@@ -3,19 +3,6 @@ from .models import Post
 
 #Adapted from http://tutorial.djangogirls.org/en/django_forms/index.html, March 6, 2016
 class PostForm(forms.ModelForm):
-	POST_TYPE_CHOICES = (
-		('MD', 'Markdown'),
-		('PT', 'Plain text'),
-	)
-	PRIVACY_CHOICES = (
-		('PV', 'Private to me'),
-		('PA', 'Private to another author'),
-		('PF', 'Private to my friends'),
-		('FOAF', 'Friend of a friend'),
-		('HF', 'Private to only friends on my host'),
-		('PB', 'Public'),
-	)
-	post_text = forms.TextField(label='Content')
-	post_title = forms.CharField(label='Title', max_length=64)
-	post_type = forms.CharField(label='Type', max_length=2, choices=POST_TYPE_CHOICES,default='PT')
-	privacy = forms.CharField(label='Privacy',max_length=4, choices=PRIVACY_CHOICES,default='PB')
+	class Meta:
+		model = Post
+		fields = ('post_text', 'post_title', 'post_type', 'privacy',)
--- social_dist/posts/views.py
+++ social_dist/posts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import PostForm
+from .models import Author
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 def index(request):
 	return HttpResponse("Hello you're at posts index")
@@ -7,10 +11,11 @@ def index(request):
 #adapted from http://tutorial.djangogirls.org/en/django_forms/index.html
 def post_new(request):
 	if request.method == "POST":
-		form = PostForm(request.POST)
+		form = PostForm(data=request.POST)
+		print(form.errors)
 		if form.is_valid():
 			post = form.save(commit=False)
-			post.author = request.user
+			post.author = Author.objects.get(user=request.user)
 			post.pub_date = timezone.now()
 			post.save()
 	else:
