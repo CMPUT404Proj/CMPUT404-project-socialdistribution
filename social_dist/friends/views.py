e52347a055c4a84edf1cd7590f4d06d8d1d6f650
rkdhatt <rkdhatt@ualberta.ca>
2016-03-07 18:04:17 -0700

    Working on creating a search thing for friends. Added a search url in friends/urls.py. In progress.

--- social_dist/friends/views.py
+++ social_dist/friends/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from authors.models import Author
+from authors.models import Author, GlobalAuthor
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -17,3 +17,32 @@ def index(request):
 	context['global_friends'] = author.getGlobalFriends()
 
 	return render(request, 'friends/index.html', context)
+
+@login_required
+def search(request):
+	# TODO: not finished with this yet.
+	if request.method == 'POST':
+		search_id = request.POST.get('search_field', None)
+		try:
+			# Get all local users.
+			users = User.objects.filter(username__icontains=search_id)
+			local_names = []
+			for user in users:
+				local_names.append(user.username)
+
+			# Get all Remote users.
+			global_users = GlobalAuthor.objects.filter(global_author_name__icontains=search_id)
+			global_names = []
+			for user in global_users:
+				global_names.append(user.global_author_name)
+
+			# Return all usernames
+			usernames = dict()
+			usernames["local_names"] = local_names
+			usernames["global_names"] = global_names
+
+			return HttpResponse(usernames)
+		except User.DoesNotExist:
+			return HttpResponse("No such user found.")
+	else:
+		return render(request, 'friends/index.html')
