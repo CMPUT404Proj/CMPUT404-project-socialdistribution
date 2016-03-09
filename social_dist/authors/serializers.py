2175db83305b4447fb9b1cbc746ccf7bcfd28cf3
kathb <kzb1895@gmail.com>
2016-03-08 18:04:26 -0700

    saved my db

--- social_dist/authors/serializers.py
+++ social_dist/authors/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from authors.models import Author
 
 class AuthorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = ('author_id','user','url','host','profile_pic')
+	displayName = serializers.SerializerMethodField('get_username')
+	id = serializers.SerializerMethodField('get_author_id')
+
+	def get_username(self, obj):
+		return obj.user.username
+
+	def get_author_id(self, obj):
+		return obj.author_id
+
+	class Meta:
+		model = Author
+		fields = ('id','host','displayName','url', 'github')
