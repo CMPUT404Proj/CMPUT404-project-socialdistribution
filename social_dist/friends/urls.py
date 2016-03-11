9a2e493b22dad2957b9f250a251849ffb75e258e
rkdhatt <rkdhatt@ualberta.ca>
2016-03-10 18:45:09 -0700

    Can delete local and remote friends now!

--- social_dist/friends/urls.py
+++ social_dist/friends/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^search/', views.search, name='search'),
+	url(r'^delete_local/(?P<author_id>[0-9a-z-]+)/', views.deleteLocalFriend, name='delete_local'),
+	url(r'^delete_global/(?P<global_author_id>[0-9a-z-]+)/', views.deleteGlobalFriend, name='delete_global'),
 ]
